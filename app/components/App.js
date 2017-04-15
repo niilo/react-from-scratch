eba776f63da3a630f5b3fb02afe6a7687d67ed64
Niilo Ursin <niilo.ursin@gmail.com>
2017-04-15 17:26:47 +0300

    react router

--- app/components/App.js
+++ app/components/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Home from './Home'
+import Nav from './Nav'
+import Battle from './Battle'
 import Popular from './Popular'
 require('../styles.css')
 
 class App extends Component {
   render () {
     return (
-      <div className='container'>
-        <Popular />
-      </div>
+      <BrowserRouter>
+        <div className='container'>
+          <Nav />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/battle' component={Battle} />
+            <Route path='/popular' component={Popular} />
+            <Route
+              render={() => {
+                return <p>Not found</p>
+              }}
+            />
+          </Switch>
+        </div>
+      </BrowserRouter>
     )
   }
 }
