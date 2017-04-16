2ab9725a3bdfb4d9f94218829f9ac341511f6437
Niilo Ursin <niilo.ursin@gmail.com>
2017-04-16 17:57:52 +0300

    axios ans promises

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,5 +1,62 @@
 import Axios from 'axios'
 
+export function battle (players) {
+  return Axios.all([players.map(getUserData)])
+    .then(sortPlayers)
+    .catch(handleError)
+}
+
+function getProfile (username) {
+  return Axios.get('https://api.github.com/users/' + username).then(user => {
+    console.log(user.data)
+    return user.data
+  })
+}
+
+function getRepos (username) {
+  return Axios.get(
+    'https://api.github.com/users/' + username + '/repos' + '?per_page=100'
+  )
+}
+
+function getStartCount (repos) {
+  return repos.data.reduce(
+    (count, repo) => {
+      return count + repo.stargazers_count
+    },
+    0
+  )
+}
+
+function calculateScore (profile, repos) {
+  const followers = profile.followers
+  const totalStars = getStartCount(repos)
+  return followers * 3 + totalStars
+}
+
+function handleError (error) {
+  console.warn(error)
+  return null
+}
+
+function getUserData (player) {
+  return Axios.all([getProfile(player), getRepos(player)]).then(data => {
+    const profile = data[0]
+    const repos = data[1]
+
+    return {
+      profile: profile,
+      score: calculateScore(profile, repos)
+    }
+  })
+}
+
+function sortPlayers (players) {
+  return players.sort((a, b) => {
+    return b.score - a.score
+  })
+}
+
 export function fetchPopularRepos (language) {
   const encodedURI = window.encodeURI(
     'https://api.github.com/search/repositories?q=stars:>1+language:' +
